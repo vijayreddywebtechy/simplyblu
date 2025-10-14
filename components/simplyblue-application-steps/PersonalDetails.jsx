b47bea31efab3949c3f002f4a123d551308393ae
Nandini <nandini.bandreddi@conscendo.io>
2025-10-14 20:01:37 +0530

    changes in personal details

--- components/simplyblue-application-steps/PersonalDetails.jsx
+++ components/simplyblue-application-steps/PersonalDetails.jsx
@@ -6,6 +6,7 @@ import ReactSelect from "../ui/ReactSelect";
 
 const PersonalDetails = ({ onNext }) => {
   const [framework, setFramework] = useState(null);
+  const [citizenship, setCitizenship] = useState(null);
 
   const options = [
     { value: "react", label: "React" },
@@ -16,90 +17,90 @@ const PersonalDetails = ({ onNext }) => {
   return (
     <div className="border border-gray-200 rounded-xl">
       <div className="p-4">
-      <h3 className="font-bspro text-xl font-medium text-gray-600 mb-8">
-        Personal Details
-      </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 gap-x-12">
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            NAME (as per ID) NAME (as per ID){" "}
-          </label>
-          <CustomInput
-            placeholder="Enter your first name"
-            value=""
-            onChange={(e) => setValue(e.target.value)}
-          />
+        <h3 className="font-bspro text-xl font-medium text-gray-600 mb-8">
+          Personal Details
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 gap-x-12">
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              NAME (as per ID) NAME (as per ID){" "}
+            </label>
+            <CustomInput
+              placeholder="Enter your first name"
+              // value=""
+              // onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              SURNAME (as per ID)
+            </label>
+            <CustomInput
+              placeholder="Enter your surname"
+              // value=""
+              // onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              ID NUMBER
+            </label>
+            <CustomInput
+              type="number"
+              placeholder="Enter your ID number"
+              // value=""
+              // onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              CELL NUMBER
+            </label>
+            <CustomInput
+              type="number"
+              placeholder="Enter your phone number"
+              // value=""
+              // onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              CELL NUMBER
+            </label>
+            <CustomInput
+              type="email"
+              placeholder="Force@gmail.com"
+              // value=""
+              // onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              NATIONALITY
+            </label>
+            <ReactSelect
+              label="Choose Framework"
+              options={options}
+              value={framework}
+              onChange={setFramework}
+              placeholder="Please Select"
+              required
+            />
+          </div>
+          <div>
+            <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
+              COUNTRY OF CITIZENSHIP
+            </label>
+            <ReactSelect
+              label="Choose Framework"
+              options={options}
+              value={citizenship}
+              onChange={setCitizenship}
+              placeholder="Please Select"
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            SURNAME (as per ID)
-          </label>
-          <CustomInput
-            placeholder="Enter your surname"
-            value=""
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            ID NUMBER
-          </label>
-          <CustomInput
-            type="number"
-            placeholder="Enter your ID number"
-            value=""
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            CELL NUMBER
-          </label>
-          <CustomInput
-            type="number"
-            placeholder="Enter your phone number"
-            value=""
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            CELL NUMBER
-          </label>
-          <CustomInput
-            type="email"
-            placeholder="Force@gmail.com"
-            value=""
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </div>
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            NATIONALITY
-          </label>
-          <ReactSelect
-            label="Choose Framework"
-            options={options}
-            value={framework}
-            onChange={setFramework}
-            placeholder="Please Select"
-            required
-          />
-        </div>
-        <div>
-          <label className="font-bspro text-xs font-medium text-gray-500 block mb-1.5">
-            COUNTRY OF CITIZENSHIP
-          </label>
-          <ReactSelect
-            label="Choose Framework"
-            options={options}
-            value={framework}
-            onChange={setFramework}
-            placeholder="Please Select"
-            required
-          />
-        </div>
-      </div>
       </div>
       <div className="flex justify-end mt-6 bg-slate-50 p-4">
         <button
@@ -110,7 +111,6 @@ const PersonalDetails = ({ onNext }) => {
         </button>
       </div>
     </div>
-
   );
 };
 
